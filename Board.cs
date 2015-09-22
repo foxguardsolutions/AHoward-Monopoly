78d43b3b2acb58e49b7c56a549498d70dc735f5c
Austin Howard <ahoward@foxguardsolutions.com>
2015-09-22 13:08:57 -0400

    Add initial Board and BoardTests classes

---
+++ Board.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Monopoly
+{
+    public class Board
+    {
+        private readonly string[] _propertyNames =
+        {
+            "Go",
+            "Mediterranean Avenue",
+            "Community Chest",
+            "Baltic Avenue",
+            "Income Tax",
+            "Reading RailRoad",
+            "Oriental Avenue",
+            "Chance",
+            "Vermont Avenue",
+            "Connecticut Avenue",
+            "Jail",
+            "St. Charles Place",
+            "Electric Company",
+            "States Avenue",
+            "Virginia Avenue",
+            "Pennsylvania Railroad",
+            "St. James Place",
+            "Community Chest",
+            "Tennessee Avenue",
+            "New York Avenue",
+            "Free Parking",
+            "Kentucky Avenue",
+            "Chance",
+            "Indiana Avenue",
+            "Illinois Avenue",
+            "B. & O. Railroad",
+            "Atlantic Avenue",
+            "Ventnor Avenue",
+            "Water Works",
+            "Marvin Gardens",
+            "Go To Jail",
+            "Pacific Avenue",
+            "North Carolina Avenue",
+            "Community Chest",
+            "Pennsylvania Avenue",
+            "Short Line Railroad",
+            "Chance",
+            "Park Place",
+            "Luxury Tax",
+            "Boardwalk",
+        };
+        private List<Property> properties = new List<Property>();
+
+        public int PropertyCount
+        {
+            get { return properties.Count; }
+        }
+
+        public string[] PropertyNames
+        {
+            get { return _propertyNames; }
+        }
+
+        public Board()
+        {
+            foreach (var name in PropertyNames)
+            {
+                properties.Add(new Property(name));
+            }
+        }
+
+        public string GetPropertyName(int index)
+        {
+            return properties[index].Name;
+        }
+    }
+}
---
+++ BoardTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Monopoly
+{
+    [TestFixture]
+    public class BoardTests
+    {
+        [Test]
+        public void BoardInitializes40PropertiesUponInstantiation()
+        {
+            Board board = new Board();
+            Assert.AreEqual(40, board.PropertyCount);
+        }
+
+        [Test]
+        public void PropertiesAreInitializedWithNamesInOrder()
+        {
+            Board board = new Board();
+            for (int index = 0; index < board.PropertyCount; index++)
+            {
+                Assert.AreEqual(board.PropertyNames[index], board.GetPropertyName(index));
+            }
+        }
+    }
+}
